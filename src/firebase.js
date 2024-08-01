f737c6eae19a86cd6fe8aa063dfb2db831fc3430
abdurctf <asharif1@usf.edu>
2024-07-31 21:44:35 -0400

    implemented env

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,18 @@
-// Import the functions you need from the SDKs you need
+// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC8PcqyfG-9J7l9bj2nCgIyZGkwZPArZAY",
-  authDomain: "tampazakahfund.firebaseapp.com",
-  databaseURL: "https://tampazakahfund-default-rtdb.firebaseio.com",
-  projectId: "tampazakahfund",
-  storageBucket: "tampazakahfund.appspot.com",
-  messagingSenderId: "89886557365",
-  appId: "1:89886557365:web:38c4c1c479dc7e3ff9eafc",
-  measurementId: "G-T75CS3GT7J"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
